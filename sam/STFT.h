c2220b05ec521579977aeaea8a06dcb67f25d656
Scott McCoid <mccoid.scott@gmail.com>
2013-02-03 16:42:53 -0500

    finishing up stft 'class'

--- sam/STFT.h
+++ sam/STFT.h
@@ -9,6 +9,36 @@
 #ifndef sam_STFT_h
 #define sam_STFT_h
 
+#include "FFT.h"
+#include "PhaseVocoder.h"
+
+enum FrameType
+{
+    BASIC,
+    PHASE_VOC
+};
+
+typedef struct t_stft
+{
+    FFT*            fft;
+    int             overlapAmount;
+    int             length;
+    int             windowSize;
+    float*          tempAudioBuffer;
+    
+    PhaseVocoder**  phaseBuffer;
+    
+} STFT;
+
+// Constructor
+STFT* newSTFT(FFT* fft, int windowSize, int overlapAmount, int sftfLength, enum FrameType type);
+
+// Destructor
+void freeSTFT(STFT* stft);
+
+//
+void compute(STFT* stft, float* buffer);
+
 
 
 #endif
